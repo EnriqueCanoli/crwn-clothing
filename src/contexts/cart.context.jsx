127a48f329682b16a35bc0c584de806dfd1667dc
EnriqueCanoli <enriquecanoli96@gmail.com>
2024-06-21 12:59:55 -0600

    reducers hook

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,7 @@
-import { createContext, useEffect, useState } from 'react';
+/**Using Reducer */
+import { type } from '@testing-library/user-event/dist/type';
+import { createContext, useReducer} from 'react';
+import { createAction } from '../utils/firebase/reducer/reducer.utils';
 
 export const addCartItem = (cartItems, productToAdd) => {
     const existingCartItem = cartItems.find(
@@ -30,6 +33,169 @@ export const removeCartItem = (cartItems, cartItemToRemove) => {
         return cartItems.map((cartItem) =>
             cartItem.id === cartItemToRemove.id
                 ? { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItem
+        );
+    }
+}
+
+
+const clearCartItem = (cartItems, cartItemToClear) =>
+    cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
+export const CartContext = createContext({
+    isCartOpen: false,
+    setIsOpen: () => { },
+    cartItems: [],
+    addItemToCart: () => { },
+    removeItemFromCart: () => { },
+    clearItemFromCart: () => { },
+    cartCount: 0,
+    cartTotal: 0
+});
+/**
+ * ACTIONS, to dont make mistake when we typed ot
+ */
+
+const CART_ACTION_TYPES = {
+    SET_CART_ITEMS: 'SET_CART_ITEMS',
+    SET_IS_CART_OPEN: 'SET_IS_CART_OPEN'
+}
+
+/**This initial state just gives us the object that we need to keep track of when it comes to 
+ * what our actual reducer should return
+ */
+const INITIAL_STATE = {
+    cartItems: [],
+    cartCount: 0,
+    cartTotal: 0,
+    isCartOpen: false
+}
+
+
+/**
+ * 
+ * The reducer should not handle any business logic
+ * State represent the current state of my application
+ * Action is an object that describes wahr chanfe should happen
+ *          -type: tells the reducer what type of action to perform
+ *          -payload: carries the data for updating (in this case, payload is an object (update: cartitems,cartcount and carttotal))
+ *  
+ * */
+const cardReducer = (state, action) =>{
+    const {type, payload} = action;
+
+    
+
+    switch(type){
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return{
+                ...state,
+                ...payload
+            }
+        case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+            return {
+                ...state,
+                isCartOpen:payload
+            }
+        default:
+            throw new Error(`Unhandled type of ${type} in cartReducer`)
+    }
+
+}
+
+export const CartProvider = ({ children }) => {
+    /**
+     * Returns 2 elements:
+     *      -the first is an array with the current state values,
+     *      -The second one is a function used to dispatch action to the reducer
+     */
+    const [{cartItems, cartCount,cartTotal, isCartOpen},dispatch] = useReducer(cardReducer,INITIAL_STATE);
+
+    /**
+     * 
+     * When dispatch is called, React invokes the cardReducer, passing the current state and the action.
+     * When an action is dispatched using dispatch, React re-evaluates the component and re-renders it if the state has changed
+     * This function is called when the use add an item (addItemToCart ) remove an item (removeItemFromCart)
+     */
+    const updateCartItemsReduer = (newCartItems) => {
+        const newCartCount = newCartItems.reduce((total, carItem) => total + carItem.quantity, 0)
+
+        const newCartTotal = newCartItems.reduce((total, carItem) => total + carItem.quantity * carItem.price, 0)
+
+        dispatch({type: CART_ACTION_TYPES.SET_CART_ITEMS,payload:{cartItems: newCartItems, cartTotal: newCartTotal, cartCount:newCartCount}})
+        //with tue reducer.utils.js we can do the same as the last function but more clearly
+        //dispatch(createAction(CART_ACTION_TYPES.SET_CART_ITEMS,{cartItems: newCartItems, cartTotal: newCartTotal, cartCount:newCartCount} ));
+    }
+    
+    const addItemToCart = (product) =>{
+        const newCartItems = addCartItem(cartItems, product);
+        updateCartItemsReduer(newCartItems)
+    }
+        
+
+    const removeItemFromCart = (cartItemToRemove) => {
+        const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+        updateCartItemsReduer(newCartItems)
+    }
+
+    const clearItemFromCart = (cartItemToClear) => {
+        const newCartItems =clearCartItem(cartItems, cartItemToClear);
+        updateCartItemsReduer(newCartItems)
+    }
+
+    const setIsCartOpen = (bool) => {
+        dispatch({type: CART_ACTION_TYPES.SET_IS_CART_OPEN, payload:bool})
+    }
+
+    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount, removeItemFromCart, clearItemFromCart, cartTotal };
+
+    return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+/* using useState
+import { createContext, useEffect, useState } from 'react';
+
+export const addCartItem = (cartItems, productToAdd) => {
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    );
+
+    if (existingCartItem) {
+        return cartItems.map((cartItem) =>
+            cartItem.id === productToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
+    }
+
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+export const removeCartItem = (cartItems, cartItemToRemove) => {
+    //find the cart item to remove
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+    //check if quantity is equal to 1, if it is remove that item from the cart
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+    //return back caritems with matching cart items with reduced quantity
+    if (existingCartItem) {
+        return cartItems.map((cartItem) =>
+            cartItem.id === cartItemToRemove.id
+            /*  ? { ...cartItem, quantity: cartItem.quantity - 1 }
                 : cartItem
         );
     }
@@ -82,3 +248,4 @@ export const CartProvider = ({ children }) => {
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
+*/
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,86 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/firebase/reducer/reducer.utils";
+
+
+/**
+ *  This context will hold the current user state and a function to update 
+ * This default value is used by React if a component consuming the context is rendered outside of a provider. 
+ */
+export const UserContext = createContext({
+    setCurrentUser: () => null,
+    currentUser: null,
+});
+
+/**
+ * This defines the action types for the reducer
+ */
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+/**
+ * This is the initial state of the context, where currentUser is initially null.
+ * The INITIAL_STATE is used by the useReducer hook to define the initial state of the reducer. 
+ */
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
+/**
+ * This is the reducer function. It takes the current state and an action as arguments, and returns a new state based on the action type. 
+ */
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return { ...state, currentUser: payload };
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+};
+
+export const UserProvider = ({ children }) => {
+
+    /**
+     * how to utilize our useReducer
+     * It takes to values (reducer, State's initial value)
+     * 
+     * The ruducer give us back to values, th state object and dispatch function
+     * Dispatch is a function that whenever you call it, you pass it an action object
+     *  Hence, dispatch will call the actions that you need
+    */
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+    const setCurrentUser = (user) =>
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            if (user) {
+                createUserDocumentFromAuth(user);
+            }
+            setCurrentUser(user);
+        });
+
+        return unsubscribe;
+    }, []);
+
+    console.log(currentUser);
+
+    const value = {
+        currentUser,
+    };
+
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+
+/**
+ * Using context
+
+import { createContext, useState, useEffect, useReducer } from "react";
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 
 //as the actual value you want to access
@@ -23,7 +105,7 @@ export const UserContext = createContext({
  * 
  * this provider is where it's going to receive the value which is going to hold the actual contextual values
  */
-export const UserProvider = ({ children }) => {
+/*export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const value = { currentUser, setCurrentUser };
 
@@ -44,3 +126,6 @@ export const UserProvider = ({ children }) => {
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+
+ */
